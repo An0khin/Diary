432bbffd6342938f377590bcfd4f54285b595dde
An0khin <anohin04@mail.ru>
2023-05-19 04:15:48 +0500

    Refactor methods of SettingButtonListener

--- src/main/java/com/home/viewmodel/listeners/SettingButtonListener.java
+++ src/main/java/com/home/viewmodel/listeners/SettingButtonListener.java
@@ -3,51 +3,46 @@ package com.home.viewmodel.listeners;
 import com.home.viewmodel.Diary;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SettingButtonListener implements ActionListener {
+    private final Diary diary;
 
-	private Diary diary;
-	
-	public SettingButtonListener(Diary diary) {
-		this.diary = diary;
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-			
-		JPanel mySqlSetting = new JPanel(new BorderLayout());
-		JLabel mySqlText = new JLabel("Use MySQL");
-		JButton mySqlButton = new JButton("OFF");
-		
-		JButton resetDataButton = new JButton("RESET ALL DATA");
-		
-		mySqlButton.addActionListener((ev) -> {
-			diary.executeUseMySql();
-			if(mySqlButton.getText().equals("OFF"))
-				mySqlButton.setText("ON");
-			else
-				mySqlButton.setText("OFF");
-		});
-		
-		resetDataButton.addActionListener((ev) -> {
-			diary.resetAllData();
-		});
-		
-		mySqlSetting.add(mySqlText, BorderLayout.WEST);
-		mySqlSetting.add(mySqlButton, BorderLayout.EAST);
-		
-		Object[] message = {
-				mySqlSetting,
-				resetDataButton
-		};
-		
-		JOptionPane pane = new JOptionPane(message, JOptionPane.PLAIN_MESSAGE, JOptionPane.PLAIN_MESSAGE);
-		JDialog dialog = pane.createDialog("SETTINGS");
-		
-		pane.remove(pane.getComponents().length - 1);
-		dialog.setVisible(true);	
-	}
+    public SettingButtonListener(Diary diary) {
+        this.diary = diary;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object[] message = setupMessage();
+
+        openDialog(message);
+    }
+
+    private Object[] setupMessage() {
+        JButton resetDataButton = setupResetButton();
+
+        return new Object[] {
+                resetDataButton
+        };
+    }
+
+    private JButton setupResetButton() {
+        JButton resetDataButton = new JButton("RESET ALL DATA");
+
+        resetDataButton.addActionListener((ev) -> {
+            diary.resetAllData();
+        });
+
+        return resetDataButton;
+    }
+
+    private void openDialog(Object[] message) {
+        JOptionPane pane = new JOptionPane(message, JOptionPane.PLAIN_MESSAGE, JOptionPane.DEFAULT_OPTION);
+        JDialog dialog = pane.createDialog("SETTINGS");
+
+        pane.remove(pane.getComponents().length - 1);
+        dialog.setVisible(true);
+    }
 }
